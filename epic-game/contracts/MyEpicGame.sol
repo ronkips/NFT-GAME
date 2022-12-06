6f60100bb7d6f9eb89afe92a5877f3b6501ff806
ronkips <kipshillash@gmail.com>
2022-12-06 11:47:15 +0300

    updated the smart contract

--- epic-game/contracts/MyEpicGame.sol
+++ epic-game/contracts/MyEpicGame.sol
@@ -1,3 +1,49 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
+import "hardhat/console.sol";
+
+contract MyEpicGame {
+    struct CharacterAttributes {
+        uint characterIndex;
+        string name;
+        string imageURI;
+        uint kip;
+        uint maxKip;
+        uint attackDamage;
+    }
+
+    // A lil array to help us hold the default data for our characters.
+    // This will be helpful when we mint new characters and need to know
+
+    CharacterAttributes[] defaultCharacters;
+
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        uint[] memory characterKip,
+        uint[] memory characterAttackDmg
+    ) {
+        // Loop through all the characters, and save their values in our contract so
+        // we can use them later when we mint our NFTs.
+        for (uint i = 0; i < characterNames.length; i += 1) {
+            defaultCharacters.push(
+                CharacterAttributes({
+                    characterIndex: i,
+                    name: characterNames[i],
+                    imageURI: characterImageURIs[i],
+                    kip: characterKip[i],
+                    maxKip: characterKip[i],
+                    attackDamage: characterAttackDmg[i]
+                })
+            );
+            CharacterAttributes memory c = defaultCharacters[i];
+            console.log(
+                "Done initializing %s w/ KIP %s, img %s",
+                c.name,
+                c.kip,
+                c.imageURI
+            );
+        }
+    }
+}
---
+++ epic-game/scripts/deploy.js
@@ -0,0 +1,22 @@
+const hardhat = require("hardhat");
+
+const main = async () => {
+  const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
+  const gameContract = await gameContractFactory.deploy(
+    ["", "", ]
+  );
+  await gameContract.deployed();
+  console.log("Contract deployed to:", gameContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
