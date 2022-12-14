c293284d441e3fb34070c56eaa5977fff64d3e49
ronkips <kipshillash@gmail.com>
2022-12-14 10:41:02 +0300

    update the smart contract on the attack function

--- epic-game/contracts/MyEpicGame.sol
+++ epic-game/contracts/MyEpicGame.sol
@@ -1,9 +1,19 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
+
+// Helper we wrote to encode in Base64
+import "./libraries/Base64.sol";
+
+// NFT contract to inherit from.
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// Helper functions OpenZeppelin provides.
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 import "hardhat/console.sol";
 
-contract MyEpicGame {
+contract MyEpicGame is ERC721 {
     struct CharacterAttributes {
         uint characterIndex;
         string name;
@@ -13,17 +23,55 @@ contract MyEpicGame {
         uint attackDamage;
     }
 
+    struct BigBoss {
+        string name;
+        string imageURI;
+        uint kip;
+        uint maxKip;
+        uint attackDamage;
+    }
+    // The tokenId is the NFTs unique identifier, it's just a number that goes
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    BigBoss public bigBoss;
+
     // A lil array to help us hold the default data for our characters.
     // This will be helpful when we mint new characters and need to know
 
     CharacterAttributes[] defaultCharacters;
 
+    // We create a mapping from the nft's tokenId => that NFTs attributes.
+    mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+
+    // A mapping from an address => the NFTs tokenId. Gives me an ez way
+    // to store the owner of the NFT and reference it later.
+    mapping(address => uint256) public nftHolders;
+
     constructor(
         string[] memory characterNames,
         string[] memory characterImageURIs,
         uint[] memory characterKip,
-        uint[] memory characterAttackDmg
-    ) {
+        uint[] memory characterAttackDmg,
+        string memory bossName,
+        string memory bossImageURI,
+        uint bossKip,
+        uint bossAttackDamage
+    ) ERC721("KIP", "HKP") {
+        //initialize the boss . save it to our global "bigBoss" variable .
+        bigBoss = BigBoss({
+            name: bossName,
+            imageURI: bossImageURI,
+            kip: bossKip,
+            maxKip: bossKip,
+            attackDamage: bossAttackDamage
+        });
+        console.log(
+            "Done initializing boss %s w/ KIP %s, img %s",
+            bigBoss.name,
+            bigBoss.kip,
+            bigBoss.imageURI
+        );
+
         // Loop through all the characters, and save their values in our contract so
         // we can use them later when we mint our NFTs.
         for (uint i = 0; i < characterNames.length; i += 1) {
@@ -45,5 +93,125 @@ contract MyEpicGame {
                 c.imageURI
             );
         }
+        // I increment _tokenIds here so that my first NFT has an ID of 1.
+        _tokenIds.increment();
+    }
+
+    // Users would be able to hit this function and get their NFT based on the
+    // characterId they send in!
+    function mintCharacterNFT(uint _characterIndex) external {
+        // Get current tokenId (starts at 1 since we incremented in the constructor).
+        uint256 newItemId = _tokenIds.current();
+
+        // Assigning the tokenId to the caller's wallet address
+        _safeMint(msg.sender, newItemId);
+        // Holding dynamic data on NFT
+        // We map the tokenId => their character attributes
+        nftHolderAttributes[newItemId] = CharacterAttributes({
+            characterIndex: _characterIndex,
+            name: defaultCharacters[_characterIndex].name,
+            imageURI: defaultCharacters[_characterIndex].imageURI,
+            kip: defaultCharacters[_characterIndex].kip,
+            maxKip: defaultCharacters[_characterIndex].maxKip,
+            attackDamage: defaultCharacters[_characterIndex].attackDamage
+        });
+
+        console.log(
+            "Minted NFT w/ tokenId %s and characterIndex %s",
+            newItemId,
+            _characterIndex
+        );
+
+        //Keeping an easy way to see who owns the NFT
+        nftHolders[msg.sender] = newItemId;
+
+        // Increment the tokenId for the next person using it.
+        _tokenIds.increment();
+    }
+
+    //tokenURI function
+    function tokenURI(
+        uint256 _tokenId
+    ) public view override returns (string memory) {
+        CharacterAttributes memory charAttributes = nftHolderAttributes[
+            _tokenId
+        ];
+
+        string memory strKip = Strings.toString(charAttributes.kip);
+        string memory strMaxKip = Strings.toString(charAttributes.maxKip);
+        string memory strAttackDamage = Strings.toString(
+            charAttributes.attackDamage
+        );
+
+        string memory json = Base64.encode(
+            abi.encodePacked(
+                '{"name": "',
+                charAttributes.name,
+                " -- NFT #: ",
+                Strings.toString(_tokenId),
+                '", "description": "This is an NFT that lets people play in the game Metaverse player!", "image": "',
+                charAttributes.imageURI,
+                '", "attributes": [ { "trait_type": "Health Points", "value": ',
+                strKip,
+                ', "max_value":',
+                strMaxKip,
+                '}, { "trait_type": "Attack Damage", "value": ',
+                strAttackDamage,
+                "} ]}"
+            )
+        );
+
+        string memory output = string(
+            abi.encodePacked("data:application/json;base64", json)
+        );
+        return output;
+    }
+
+    function attackBoss() public {
+        // Get the state of the player's NFT.
+        uint256 nftTokenIdOfPlayer = nftHolders[msg.sender];
+        CharacterAttributes storage player = nftHolderAttributes[
+            nftTokenIdOfPlayer
+        ];
+        console.log(
+            "\nPlayer w/ character %s about to attack. Has %s KIP and %s AD",
+            player.name,
+            player.kip,
+            player.attackDamage
+        );
+        console.log(
+            "Boss %s has %s KIP and %s AD",
+            bigBoss.name,
+            bigBoss.kip,
+            bigBoss.attackDamage
+        );
+
+        // Make sure the player has more than 0 KIP.
+        require(
+            player.kip > 0,
+            "Error: character must have KIP to attack boss."
+        );
+        // Make sure the boss has more than 0 KIP.
+        require(
+            bigBoss.kip > 0,
+            "Error: boss must have KIP to attack character."
+        );
+        // Allow player to attack boss.
+        if (bigBoss.kip < player.attackDamage) {
+            bigBoss.kip = 0;
+        } else {
+            bigBoss.kip = bigBoss.kip - player.attackDamage;
+        }
+        // Allow boss to attack player.
+        if (player.kip < bigBoss.attackDamage) {
+            player.kip = 0;
+        } else {
+            player.kip = player.kip - bigBoss.attackDamage;
+        }
+        console.log("Player attacked boss. New boss kip is: %s", bigBoss.kip);
+        console.log(
+            "Boss attacked player. New player kip is: %s\n",
+            player.kip
+        );
     }
 }
--- epic-game/scripts/deploy.js
+++ epic-game/scripts/deploy.js
@@ -3,10 +3,49 @@ const hardhat = require("hardhat");
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
   const gameContract = await gameContractFactory.deploy(
-    ["", "", ]
+    ["Hilla", "Samu", "Nash", "Ken"], // Names
+    [
+      "https://pin.it/2AGSyjR",
+      "https://pin.it/4lg7rns", //Images
+      "https://pin.it/1TDqyU2",
+      "https://pin.it/1oD4aWF"
+    ],
+    [500, 700, 900, 1200], // kip values
+    [50, 80, 100, 200], //Attack damage values
+    "Hillary", //Boss name
+    "https://pin.it/17opdkP", //Boss image
+    1000000, // Boss kip
+    50 // Boss attack damage
   );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
+
+  let txn;
+  // txn = await gameContract.mintCharacterNFT(0);
+  // await txn.wait();
+  // console.log("Minted NFT #1");
+
+  // txn = await gameContract.mintCharacterNFT(1);
+  // await txn.wait();
+  // console.log("Minted NFT #2");
+
+  // txn = await gameContract.mintCharacterNFT(2);
+  // await txn.wait();
+  // console.log("Minted NFT #3");
+
+  txn = await gameContract.mintCharacterNFT(3);
+  await txn.wait();
+  txn = await gameContract.attackBoss();
+  await txn.wait();
+
+  txn = await gameContract.attackBoss();
+  await txn.wait();
+
+  console.log("Done deploying and minting!");
+  console.log("****************************************************\n");
+  // Get the value of the NFT's URI.
+  // let returnedTokenUri = await gameContract.tokenURI(2);
+  // console.log("Token URI", returnedTokenUri);
 };
 
 const runMain = async () => {
